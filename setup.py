634fcc214ff8d5893ddb90ee6e8e5eda1a3118aa
honghaohan <honghaohan@arxanfintech.com>
2018-04-20 15:54:26 +0800

    Update version to 1.5.1

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@
 from setuptools import setup, find_packages
 import io
 
+with open('./requirements.txt') as reqs_txt:
+    requirements = [line for line in reqs_txt]
+
 setup(
     name='wallet-sdk-py',
-    version='1.5.0',
+    version='1.5.1',
     description="Python SDKs for Blockchain Wallet.",
     long_description=io.open('README.md', encoding='utf-8').read(),
     url='https://github.com/arxanchain/wallet-sdk-py/',
     download_url='https://github.com/arxanchain/wallet-sdk-py/',
     packages=find_packages(),
     platforms='any',
-    install_requires=[],
-    dependency_links=[],
+    install_requires=requirements,
+    dependency_links=[
+        "git+git://github.com/arxanchain/py-common.git#egg=py-common-v1.5.1"
+    ],
     include_package_data=True,
     zip_safe=False,
 )
